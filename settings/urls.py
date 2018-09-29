d804f0525ef3d35be1d7e31c5b2ba0115bbae7b2
monetree <soubhagyakumar666@gmail.com>
2018-09-29 16:02:54 +0530

    added argon ui

--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
+
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     # url(r'^snippets/$', views.snippet_list),
     # url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
     url(r'^snippets/$', views.SnippetList.as_view()),
